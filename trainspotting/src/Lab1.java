ce6c882116b07b617b18092bda70aa9b92a6666a
William <william.hp.frisk@gmail.com>
2023-01-24 12:31:14 +0100

    24/1 division into "sections"

--- trainspotting/src/Lab1.java
+++ trainspotting/src/Lab1.java
@@ -46,7 +46,8 @@ public class Lab1 {
     }
   }
 
-  private class Train implements Runnable{
+
+  private class Train implements Runnable {
     private final int id;
     TSimInterface tsi = TSimInterface.getInstance();
     int[] rMiddleSensor = new int[]{16, 9};
@@ -55,6 +56,7 @@ public class Lab1 {
     int[] lMiddleSensor = new int[]{3, 9};
     int[] llMiddleSensor = new int[]{4, 10};
     int[] lMiddleSwitch = new int[]{4, 9};
+    Section[] sections = new Section[]{new Section(15, 9, 16, 9, 15, 10)};
     static Semaphore middleSemaphore = new Semaphore(1);
 
     Train(int id, int speed) throws CommandException{
@@ -64,26 +66,64 @@ public class Lab1 {
 
     public void run() {
       try {
+        TwoSensorSection section = null;
         while (true) {
           SensorEvent event = tsi.getSensor(id);
+          
+          for (TwoSensorSection s : sections) {
+            if (s.thisSection(event.getXpos(), event.getYpos())) {
+              section = s;
+            }
+          }
 
+          section.toggleSection();
+          /* 
           if (event.getXpos() == rMiddleSensor[0] && event.getStatus() == SensorEvent.ACTIVE) {
             if (middleSemaphore.availablePermits() == 0) {
-              tsi.setSwitch(rMiddleSwitch[0], rMiddleSwitch[1], tsi.SWITCH_LEFT);
-              tsi.setSwitch(lMiddleSwitch[0], lMiddleSwitch[1], tsi.SWITCH_RIGHT);
+              tsi.setSwitch(rMiddleSwitch[0], rMiddleSwitch[1], tsi.SWITCH_LEFT); 
+              while (true) {
+                event = tsi.getSensor(id);
+                if (event.getXpos() == rlMiddleSensor[0] && event.getStatus() == SensorEvent.INACTIVE) {
+                  tsi.setSwitch(rMiddleSwitch[0], rMiddleSwitch[1], tsi.SWITCH_RIGHT);
+                }
+                if (event.getXpos() == llMiddleSensor[0] && event.getStatus() == SensorEvent.ACTIVE) {
+                  tsi.setSwitch(lMiddleSwitch[0], lMiddleSwitch[1], tsi.SWITCH_RIGHT);
+                }
+              }
+            
             } else {
               middleSemaphore.acquire();
+              while (true) {
+                event = tsi.getSensor(id);
+                if (event.getXpos() == lMiddleSensor[0] && event.getStatus() == SensorEvent.ACTIVE) {
+                  middleSemaphore.release();
+                }
+              }
             }
           }
 
           else if (event.getXpos() == lMiddleSensor[0] && event.getStatus() == SensorEvent.ACTIVE) {
             if (middleSemaphore.availablePermits() == 0) {
               tsi.setSwitch(lMiddleSwitch[0], lMiddleSwitch[1], tsi.SWITCH_RIGHT);
-              tsi.setSwitch(rMiddleSwitch[0], rMiddleSwitch[1], tsi.SWITCH_LEFT);
+              while (true) {
+                event = tsi.getSensor(id);
+                if (event.getXpos() == rlMiddleSensor[0] && event.getStatus() == SensorEvent.ACTIVE) {
+                  tsi.setSwitch(rMiddleSwitch[0], rMiddleSwitch[1], tsi.SWITCH_RIGHT);
+                }
+                if (event.getXpos() == llMiddleSensor[0] && event.getStatus() == SensorEvent.INACTIVE) {
+                  tsi.setSwitch(lMiddleSwitch[0], lMiddleSwitch[1], tsi.SWITCH_LEFT);
+                }
+              }
             } else {
               middleSemaphore.acquire();
+              while (true) {
+                event = tsi.getSensor(id);
+                if (event.getXpos() == rMiddleSensor[0] && event.getStatus() == SensorEvent.ACTIVE) {
+                  middleSemaphore.release();
+                }
+              }
             }
-          }
+          }*/
 
         }
       } catch (CommandException e) {
@@ -99,5 +139,98 @@ public class Lab1 {
         tsi.setSpeed(id, speed);
     }
   }
+  
+  private class TwoSensorSection{
+      private int switchX;
+      private int switchY;
+      private int sensor1X;
+      private int sensor1Y;
+      private int sensor2X;
+      private int sensor2Y;
+      private TSimInterface tsi = TSimInterface.getInstance();
+      private int trainId;
+      private Semaphore semaphore;
+
+      public TwoSensorSection(int trainId, int[] switch1, int[] sensor1, int[] sensor2, Semaphore semaphore) {
+        this.switchX = switch1[0];
+        this.switchY = switch1[1];
+        this.sensor1X = sensor1[0];
+        this.sensor1Y = sensor1[1];
+        this.sensor2X = sensor2[0];
+        this.sensor2Y = sensor2[1];
+        this.semaphore = semaphore;
+      }
+
+      private boolean thisSection(int x, int y) {
+        return (sensor1X == x && sensor1Y == y) || (sensor2X == x && sensor2Y == y);
+      }
+
+      private void toggleSection (int x, int y) throws CommandException, InterruptedException {
+        if (semaphore.tryAcquire()) {
+
+        } else if (sensor1X == x && sensor1Y == y) {
+          makeSwitch();
+        } else if (sensor2X == x && sensor2Y == y) {
+          reverseSwitch();
+        }
+      }
+
+      private boolean tryAcquire() {
+        return semaphore.tryAcquire();
+      }
+
+      private void reverseSwitch() throws CommandException, InterruptedException {
+        tsi.setSwitch(switchX, switchY, tsi.SWITCH_LEFT); 
+        while (true) {
+          SensorEvent event = tsi.getSensor(trainId);
+          if (event.getXpos() == sensor1X && event.getStatus() == SensorEvent.ACTIVE) {
+            tsi.setSwitch(switchX, switchY, tsi.SWITCH_RIGHT);
+          }
+        }
+      }
+
+      private void makeSwitch() throws CommandException, InterruptedException {
+        tsi.setSwitch(switchX, switchY, tsi.SWITCH_LEFT); 
+        while (true) {
+          SensorEvent event = tsi.getSensor(trainId);
+          if (event.getXpos() == sensor2X && event.getStatus() == SensorEvent.INACTIVE) {
+            tsi.setSwitch(switchX, switchY, tsi.SWITCH_RIGHT);
+          }
+        }
+      }
+    }
+    /* 
+    private class FourSensorSection extends TwoSensorSection{
+      private int sensor3X;
+      private int sensor3Y;
+      private int sensor4X;
+      private int sensor4Y;
+      private int switch2X;
+      private int switch2Y;
+
+      public FourSensorSection(int trainId, int[] switch1, int[] switch2, int[] sensor1, int[] sensor2, int[] sensor3, int[] sensor4) {
+        super(trainId, switch1, sensor1, sensor2);
+        this.sensor3X = sensor3[0];
+        this.sensor3Y = sensor3[1];
+        this.sensor4X = sensor4[0];
+        this.sensor4Y = sensor4[1];
+        this.switch2X = switch2[0];
+        this.switch2Y = switch2[1];
+      }
+
+      private void makeSwitch() throws CommandException, InterruptedException {
+
+        tsi.setSwitch(switchX, switchY, tsi.SWITCH_LEFT); 
+        while (true) {
+          SensorEvent event = tsi.getSensor(trainId);
+          if (event.getXpos() == sensor2X && event.getStatus() == SensorEvent.INACTIVE) {
+            tsi.setSwitch(switchX, switchY, tsi.SWITCH_RIGHT);
+          }
+        }
+      }
+      
+
+    }
+    */
  
 }
